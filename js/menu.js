a0e3f25c1d7561ab71d3e58098a464e538a05119
BrCasagrande <brunopcasagrande@gmail.com>
2018-04-06 23:55:57 -0300

    Adicionando screen width a função toogleMenu

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,23 @@
 var btn = document.getElementsByClassName("toogle");
+let scr = screen.width;
 
-for(let i=0; i < btn.length; i++){
-	btn[i].addEventListener("click",toogleMenu,false);
-}
 
+	for(let i=0; i < btn.length; i++){
+		btn[i].addEventListener("click",toogleMenu,false);
+	}
+	
 
 function toogleMenu() {
 	let menuHeight = document.getElementById("menu").offsetHeight;
-
-	if(menuHeight == "0"){
-	    document.getElementById("menu").style.height = "auto";
-	    document.getElementById("menu").style.opacity = "1";
-	} else {
-		document.getElementById("menu").style.height = "0";
-	    document.getElementById("menu").style.opacity = "0";
-	    
+	if (scr < 1200) {
+		if(menuHeight == "0"){
+		    document.getElementById("menu").style.height = "auto";
+		    document.getElementById("menu").style.opacity = "1";
+		} else {
+			document.getElementById("menu").style.height = "0";
+		    document.getElementById("menu").style.opacity = "0";
+		    
+		}
 	}
-
 }
 
